c6da6bb4a87861582d9e3e035f157ee4ec976804
SY Han <elvishan2008@gmail.com>
2022-02-20 21:08:17 +0900

    스프링일 때 ComponentScan로 클래스를 읽어와서 빈 주임

--- src/main/java/me/springcore/magicdobby/springapplicationcontext/ApplicationConfig.java
+++ src/main/java/me/springcore/magicdobby/springapplicationcontext/ApplicationConfig.java
@@ -1,19 +1,10 @@
 package me.springcore.magicdobby.springapplicationcontext;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
 public class ApplicationConfig {
-    @Bean
-    public BookRepsitory bookRepsitory(){
-        return new BookRepsitory();
-    }
-
-    @Bean
-    public BookService bookService(){
-        BookService bookService = new BookService();
-        bookService.setBookRepsitory(bookRepsitory());
-        return bookService;
-    }
 }

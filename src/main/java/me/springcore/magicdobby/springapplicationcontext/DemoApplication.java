d980df0df0f16e7d2bae5774ab48be2890f814eb
SY Han <elvishan2008@gmail.com>
2022-02-20 21:09:37 +0900

    SpringBootApplication으로 스프링부트에서 알아서 자동 스캔과 자동 빈등록

--- src/main/java/me/springcore/magicdobby/springapplicationcontext/DemoApplication.java
+++ src/main/java/me/springcore/magicdobby/springapplicationcontext/DemoApplication.java
@@ -1,20 +1,16 @@
 package me.springcore.magicdobby.springapplicationcontext;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-        String[] beanDefNames = context.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanDefNames));
-        BookService bookService = (BookService) context.getBean("bookService");
-        System.out.println(bookService.bookRepsitory != null);
     }
 
 }
